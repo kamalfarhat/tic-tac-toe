a0c944ee57bfb257b0bcaaa04b1f3fb42d0096b2
Kamal <kamalfarhat@gmail.com>
2023-08-21 15:37:37 +0800

    Adding play again feature, and displaying results using a game class method instead of through the main body

--- game.rb
+++ game.rb
@@ -9,6 +9,12 @@ class Game
         puts "Welcome to Tic-Tac-Toe. This is a 2 player game, Player1 will be represented\nwith X, and player2 will be represented with O.\nIn order to make a move, choose a number from 1 to 9, with 1 representing\nthe 1st square, 2 representing the 2nd square, and so forth.\n"
     end
 
+    def reset()
+        @moves = ["s", "s", "s", "s", "s", "s", "s", "s", "s"]
+        @game_complete = false
+        @the_winner = ""
+    end
+
     def draw_board()
         moves_temp = @moves.map {|s| s.sub("s", " ")}
         puts  "\n " + moves_temp[0] + " " + "|".gray + " " + moves_temp[1] + " " + "|".gray + " " + moves_temp[2] + " \n" +
@@ -22,16 +28,42 @@ class Game
     def get_move(player, move)
         @moves[move.to_i-1] = player.sign
         draw_board()
-        self.win?(player.name, player.sign)
+        self.game_over?(player)
     end
 
-    def win?(player, sign)
-        if(@moves.join('').match(/#{sign}{3}\D\D\D\D\D\D|\D\D\D#{sign}{3}\D\D\D|\D\D\D\D\D\D#{sign}{3}|#{sign}\D\D#{sign}\D\D#{sign}|#{sign}\D\D\D#{sign}\D\D\D#{sign}|\D\D#{sign}\D#{sign}\D#{sign}\D\D/))
-            @the_winner = player
+    def game_over?(player)
+        if(@moves.join('').match(/#{player.sign}{3}\D\D\D\D\D\D|\D\D\D#{player.sign}{3}\D\D\D|\D\D\D\D\D\D#{player.sign}{3}|#{player.sign}\D\D#{player.sign}\D\D#{player.sign}|#{player.sign}\D\D\D#{player.sign}\D\D\D#{player.sign}|\D\D#{player.sign}\D#{player.sign}\D#{player.sign}\D\D/))
+            @the_winner = player.name
             @game_complete = true
+            self.display_result()
+            self.play_again(player)
         elsif(!@moves.join('').match("s"))
             @the_winner = "Draw"
             @game_complete = true
+            self.display_result()
+            self.play_again(player)
+        end
+    
+    end
+
+    def display_result()
+        if self.the_winner == "Draw"
+            puts "It's a Draw!".red
+        else
+            puts self.the_winner.green + " wins!".green
+        end
+    end
+
+    def play_again(player)
+        puts "Play again? (Y/N)"
+        repeat = gets
+        while !repeat.match(/Y|y|N|n/)
+            puts "Enter a valid selection (Y/N)".red
+            repeat = gets
+        end
+        if (repeat.match(/Y|y/))
+            self.reset()
+            player.reset()
         end
     end
 end
@@ -43,6 +75,14 @@ class Player
     def initialize(sign, name)
         @sign = sign
         @name = name
+    end
+
+    def reset()
+        @@all_moves = []
+    end
+
+    def self.show_all_moves
+        @@all_moves
     end
 
     def make_move()
@@ -65,14 +105,12 @@ end
 game = Game.new()
 player1 = Player.new("X", "Player1")
 player2 = Player.new("O", "Player2")
+
 while !game.game_complete 
     game.get_move(player1, player1.make_move())
-    if !game.game_complete
-        game.get_move(player2, player2.make_move())    
+    if !game.game_complete && !Player.show_all_moves.empty?
+        game.get_move(player2, player2.make_move())
+        
     end
-end
-if game.the_winner == "Draw"
-    puts "It's a Draw!".red
-else
-    puts game.the_winner.green + " wins!".green
+    puts Player.show_all_moves
 end
